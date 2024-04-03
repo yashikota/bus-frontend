a80002b743be0d47feaf61c35005d21f84d47219
yashikota <52403688+yashikota@users.noreply.github.com>
2024-04-03 09:03:45 +0900

    update ui

--- src/about.md
+++ src/about.md
@@ -1,7 +1,5 @@
 # OIT Bus App
 
-## 使いかた
-
 起動初回時に10秒ほど待つ必要があります。  
 1分毎に自動更新されます。  
 お問い合わせは[ここ](https://github.com/oit-tools/bus-frontend/issues)からお願いします。  
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,6 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import WifiTetheringIcon from "@mui/icons-material/WifiTethering";
-import WifiTetheringOffIcon from "@mui/icons-material/WifiTetheringOff";
 
 export interface Bus {
     BusStop: string;
@@ -22,7 +20,7 @@ export const CardComponent = (props: Bus) => {
         BusStop,
         Stand,
         Name,
-        IsSignal,
+        // IsSignal,
         OnTime,
         EstimatedTime,
         MoreMinutes,
@@ -41,19 +39,30 @@ export const CardComponent = (props: Bus) => {
             }}
         >
             <CardContent>
-                {IsSignal ? <WifiTetheringIcon /> : <WifiTetheringOffIcon />}
-                <Typography variant="body2" color="text.secondary">
-                    定刻 : {OnTime},{" "}
-                    {DelayMinutes === 0 ? "定時運行中" : `遅れ${DelayMinutes}`}
+                {/* {IsSignal ? <WifiTetheringIcon /> : <WifiTetheringOffIcon />} */}
+
+                <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    margin="auto"
+                    textAlign="center"
+                >
+                    {OnTime}発 [{System}] {Destination} 行き
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+
+                <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    margin="auto"
+                    textAlign="center"
+                >
                     {Stand === "OIT"
                         ? `${BusStop} → ${Name}`
                         : `${Name} → ${BusStop}`}
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    [{System}] {Destination} 行き
-                </Typography>
+
                 <Typography
                     gutterBottom
                     variant="h5"
@@ -66,6 +75,18 @@ export const CardComponent = (props: Bus) => {
                         ? "まもなく到着"
                         : `残り${MoreMinutes}`}
                 </Typography>
+
+                <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    margin="auto"
+                    textAlign="center"
+                    color="error"
+                >
+                    {DelayMinutes === 0 ? "" : `遅れ${DelayMinutes}分`}
+                </Typography>
+
             </CardContent>
         </Card>
     );
